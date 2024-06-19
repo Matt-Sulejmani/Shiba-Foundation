062178ffe0fce2519a3485a6798fb4c9fbc2a868
Matteo Sulejmani <matsttech75@gmail.com>
2024-06-19 16:14:50 +0200

    Delete backend directory

--- backend/API/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import Visitor
-
-
-class UserSerializer():
-    pass
--- backend/backend/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from API.models import Visitor, Moderator, Admin
-
-admin.site.register([Visitor, Moderator, Admin])
